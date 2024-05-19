d0e180ddc6d8bbb4b3ea3fd1fa938c09b95f366c
Asad ur Rehman <aurehman.bese21seecs@seecs.edu.pk>
2024-05-19 21:58:51 +0500

    changes

--- Client/selenium-tests/tests/test_login.py
+++ Client/selenium-tests/tests/test_login.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 from selenium import webdriver
 from pages.login_page import LoginPage
 
@@ -11,12 +12,16 @@ def driver():
 def test_valid_user_login(driver):
     login_page = LoginPage(driver)
     login_page.open()
+    time.sleep(2)
     login_page.login_as_user('testuser@example.com', 'correctpassword')
+    time.sleep(2)
     assert "Expected Title After Login" in driver.title
 
 def test_invalid_user_login(driver):
     login_page = LoginPage(driver)
     login_page.open()
+    time.sleep(2)
     login_page.login_as_user('testuser@example.com', 'wrongpassword')
+    time.sleep(2)
     assert "Some Error Occurred, Please try again!" in login_page.get_alert_message()
 
